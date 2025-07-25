97e157e7de837ce4f20ef8deca0072baa8e9fde1
adityakumartiwary <adityatiwary888@gmail.com>
2025-07-25 18:29:20 +0530

    Add new library model

--- library_mgmt/libmg_port/urls.py
+++ library_mgmt/libmg_port/urls.py
@@ -1,6 +1,9 @@
-
 from django.urls import path
 from .views import *
+
 urlpatterns = [
     path('', home, name='home'),
+    path('home/', home, name='home'),
+    path('shop', shopping_cart, name='shopping'),  # Change 'shopping' to 'shop'
+    path('save',save_student),
 ]
--- library_mgmt/libmg_port/views.py
+++ library_mgmt/libmg_port/views.py
@@ -9,3 +9,7 @@ def home(request):
 
 def shopping_cart(request):
     return HttpResponse("This is the shopping cart page")
+
+def save_student(request):
+    student_name = request.POST['student_name']
+    return render(request, "welcome.html", context={'student_name': student_name})
