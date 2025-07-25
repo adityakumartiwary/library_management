796695fd34eb8c96b163a6a91ad82b2789ce1322
adityakumartiwary <adityatiwary888@gmail.com>
2025-07-25 22:14:31 +0530

    Add new library model

--- library_mgmt/libmg_port/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LibmgPortConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'libmg_port'
--- library_mgmt/libmg_port/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', home, name='home'),  # This maps the root URL to the home view
-    path('home/', home, name='home'),
-    path('shop', shopping_cart, name='shopping'),
-    path('save', save_student),
-]
